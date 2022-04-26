38fa7b524631c6f7cf2d5c8098a51c293956eecf
Jure Vidmar <vidmarjur@gmail.com>
2022-04-26 14:34:58 +0200

    Create separate jobs for staging/production, handle config

---
+++ docusaurus.config.prod.js
@@ -0,0 +1,52 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'PRODUCTIOOOOOOON',
+  tagline: 'PRODUCTIOOOOOOON',
+  url: 'https://jurc192.github.io',
+  baseUrl: '/testdocs/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'jurc192', // Usually your GitHub org/user name.
+  projectName: 'testdocs', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        }
+      },
+      footer: {
+        style: 'dark',
+        // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
