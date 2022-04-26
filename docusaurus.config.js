5e633f22c17d6c7cf0485cabdd639774b3bd522d
Jure Vidmar <vidmarjur@gmail.com>
2022-04-26 14:48:08 +0200

    Overriding values

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,52 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const config = require('./docusaurus.config.base')
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: 'STAGINGGGGGGGGGG',
-  tagline: 'STAGINGGGGGGGGGG',
-  url: 'https://jurc192.github.io',
-  baseUrl: '/testdocs/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'jurc192', // Usually your GitHub org/user name.
-  projectName: 'testdocs', // Usually your repo name.
-
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        }
-      },
-      footer: {
-        style: 'dark',
-        // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
-};
+config.title += 'Staging'
 
 module.exports = config;
